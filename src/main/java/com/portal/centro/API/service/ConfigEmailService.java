99766bb04bb8f00d58b16fb4e9a034871c34ac47
Erick Elias Borges <50887364+erickeliasborges@users.noreply.github.com>
2023-08-30 19:21:58 -0300

    Adicionado método de buscar usuário logado no AuthService pois ao usar o do UserService no ConfigEmailService ocorre um loop de dependências ('form a cycle').

--- src/main/java/com/portal/centro/API/service/ConfigEmailService.java
+++ src/main/java/com/portal/centro/API/service/ConfigEmailService.java
@@ -6,6 +6,7 @@ import com.portal.centro.API.generic.crud.GenericService;
 import com.portal.centro.API.model.ConfigEmail;
 import com.portal.centro.API.model.User;
 import com.portal.centro.API.repository.ConfigEmailRepository;
+import com.portal.centro.API.security.auth.AuthService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,12 +16,12 @@ import java.util.Objects;
 public class ConfigEmailService extends GenericService<ConfigEmail, Long> {
 
     final ConfigEmailRepository configEmailRepository;
-    final UserService userService;
+    final AuthService authService;
 
-    public ConfigEmailService(ConfigEmailRepository configEmailRepository, UserService userService) {
+    public ConfigEmailService(ConfigEmailRepository configEmailRepository, AuthService authService) {
         super(configEmailRepository);
         this.configEmailRepository = configEmailRepository;
-        this.userService = userService;
+        this.authService = authService;
     }
 
     public ConfigEmail find() {
@@ -32,7 +33,7 @@ public class ConfigEmailService extends GenericService<ConfigEmail, Long> {
 
     @Override
     public ConfigEmail save(ConfigEmail requestBody) throws Exception {
-        User selfUser = userService.findSelfUser();
+        User selfUser = authService.findLoggedUser();
         if (!Objects.equals(selfUser.getRole(), Type.ADMIN))
             throw new ValidationException("Somente o administrador pode realizar esta ação.");
         return super.save(requestBody);
