a214b37c45b869de3d71c91c25c5577e3a56b602
Erick Elias Borges <50887364+erickeliasborges@users.noreply.github.com>
2023-09-13 20:20:36 -0300

    Cadastro de domínios para as roles e nova role parceiro.

--- src/main/java/com/portal/centro/API/utils/UtilsService.java
+++ src/main/java/com/portal/centro/API/utils/UtilsService.java
@@ -1,8 +1,10 @@
 package com.portal.centro.API.utils;
 
 import com.portal.centro.API.enums.Action;
-import com.portal.centro.API.model.Permission;
 import com.portal.centro.API.enums.Type;
+import com.portal.centro.API.model.DomainRole;
+import com.portal.centro.API.model.Permission;
+import com.portal.centro.API.repository.DomainRoleRepository;
 import com.portal.centro.API.repository.PermissionRepository;
 import org.springframework.stereotype.Component;
 
@@ -10,26 +12,24 @@ import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import java.util.*;
 
-import static java.util.stream.Collectors.toList;
-
 @Component
 public class UtilsService {
 
     private final PermissionRepository permissionRepository;
+    private final DomainRoleRepository domainRoleRepository;
 
-    public UtilsService(PermissionRepository permissionRepository) {
+    public UtilsService(PermissionRepository permissionRepository, DomainRoleRepository domainRoleRepository) {
         this.permissionRepository = permissionRepository;
+        this.domainRoleRepository = domainRoleRepository;
     }
-
 
     public String generateEmailMessage(String content) {
         return "";
     }
 
     public Type getRoleType(String email){
-
-        String[] dominiosValidos = {"professores.utfpr.edu.br", "utfpr.edu.br", "alunos.utfpr.edu.br"}; //Permitidos
-
+//        String[] dominiosValidos = {"professores.utfpr.edu.br", "utfpr.edu.br", "alunos.utfpr.edu.br"}; //Permitidos
+        List<DomainRole> validDomainRoles = domainRoleRepository.findAll();
         try {
             InternetAddress internetAddress = new InternetAddress(email);
             internetAddress.validate(); //Valida o formato do email usando as especificações RFC 5322
@@ -37,21 +37,26 @@ public class UtilsService {
                                                                                 o nome do usuário e o domínio, separados
                                                                                 pelo caractere "@". Acessando a segunda
                                                                                 String do array com [1]  */
-            for (String dominioValido : dominiosValidos) {
-                if (dominio.equals(dominioValido)) {
-                    if ("professores.utfpr.edu.br".equals(dominio) || "utfpr.edu.br".equals(dominio)){
-                        return Type.PROFESSOR;
-                    }  else {
-                       return Type.STUDENT;
-                    }
-                }
-            }
-
+//            for (DomainRole dominioValido : validDomainRoles) {
+//                if (dominio.equals(dominioValido.getDomain())) {
+//                    if ("professores.utfpr.edu.br".equals(dominio) || "utfpr.edu.br".equals(dominio)){
+//                        return Type.PROFESSOR;
+//                    }  else {
+//                       return Type.STUDENT;
+//                    }
+//                }
+//            }
+            Optional<DomainRole> domainRoleOptional =
+                    validDomainRoles
+                            .stream()
+                            .filter((domain) -> Objects.equals(domain.getDomain(), dominio))
+                            .findFirst();
+            if (domainRoleOptional.isPresent())
+                return domainRoleOptional.get().getRole();
+            return Type.EXTERNAL;
         } catch (Exception e) {
             return Type.EXTERNAL;
         }
-
-        return Type.EXTERNAL;
     }
 
     public List<Permission> getPermissionsByRole(final Type role) {
