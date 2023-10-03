d7f76e918eef58c394b2803f2afff526e681d932
Erick Elias Borges <50887364+erickeliasborges@users.noreply.github.com>
2023-10-02 21:20:49 -0300

    Ajuste imports

--- src/main/java/com/portal/centro/API/service/AuditService.java
+++ src/main/java/com/portal/centro/API/service/AuditService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class AuditService extends GenericService<Audit, Long> {
