9e7a25813478cb7ee670051845efa8b28437bcf2
GiovaneWerlang <giovanewerlang@alunos.utfpr.edu.br>
2023-11-05 17:26:02 -0300

    buscas e dto dash

--- src/main/java/com/portal/centro/API/repository/SolicitationRepository.java
+++ src/main/java/com/portal/centro/API/repository/SolicitationRepository.java
@@ -1,5 +1,6 @@
 package com.portal.centro.API.repository;
 
+import jakarta.persistence.Tuple;
 import com.portal.centro.API.enums.SolicitationStatus;
 import com.portal.centro.API.generic.crud.GenericRepository;
 import com.portal.centro.API.model.Solicitation;
@@ -7,6 +8,7 @@ import com.portal.centro.API.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -19,5 +21,36 @@ public interface SolicitationRepository extends GenericRepository<Solicitation, 
 
     List<Solicitation> findAllByStatus(SolicitationStatus status);
     Page<Solicitation> findAllByStatus(SolicitationStatus status, Pageable pageable);
+    @Query(
+            value = "select " +
+                    "ROW_NUMBER() OVER (ORDER BY status) AS id, " +
+                    "case  " +
+                    " when s.status = 0 then 'Ag. Conf. do Orientador' " +
+                    " when s.status = 1 then 'Ag. Conf. do Laboratório' " +
+                    " when s.status = 2 then 'Ag. amostra' " +
+                    " when s.status = 3 then 'Ag. Análise' " +
+                    " when s.status = 4 then 'Ag. Pagamento' " +
+                    " when s.status = 5 then 'Recusado' " +
+                    " when s.status = 6 then 'Concluído' " +
+                    " else null " +
+                    "end as label " +
+                    ", count(s.status) as value " +
+                    "from solicitation s  " +
+                    "group by s.status " +
+                    "order by s.status",
+            nativeQuery = true)
+    List<Tuple> findGraficoSolicitacoesNative();
+
+    @Query(
+            value = "select " +
+                    "ROW_NUMBER() OVER (ORDER BY equipment_id) AS id, " +
+                    "e.name as label, count(s.equipment_id) as value " +
+                    "from solicitation s " +
+                    "join equipment e on e.id = s.equipment_id " +
+                    "group by e.name, s.equipment_id  " +
+                    "order by count(s.equipment_id) desc " +
+                    "limit 10",
+            nativeQuery = true)
+    List<Tuple> findGraficoEquipamentoSolicitacaoNative();
 
 }
---
+++ src/main/java/com/portal/centro/API/service/DashboardService.java
@@ -0,0 +1,112 @@
+package com.portal.centro.API.service;
+
+import com.portal.centro.API.dto.GraficoDTO;
+import com.portal.centro.API.dto.GraficoDadoDTO;
+import com.portal.centro.API.repository.EquipmentRepository;
+import com.portal.centro.API.repository.SolicitationRepository;
+import com.portal.centro.API.repository.UserRepository;
+import jakarta.persistence.Tuple;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class DashboardService {
+
+    private final SolicitationRepository solicitationRepository;
+    private final UserRepository userRepository;
+    private final EquipmentRepository equipmentRepository;
+
+    public DashboardService(
+            SolicitationRepository solicitationRepository,
+            UserRepository userRepository,
+            EquipmentRepository equipmentRepository
+            ) {
+        this.solicitationRepository = solicitationRepository;
+        this.userRepository = userRepository;
+        this.equipmentRepository = equipmentRepository;
+    }
+
+    public GraficoDTO getGraficoSolicitacao(){
+        List<Tuple> dados = solicitationRepository.findGraficoSolicitacoesNative();
+        List<GraficoDadoDTO> lista = new ArrayList<>();
+        for (Tuple tuple : dados){
+            lista.add(
+                    new GraficoDadoDTO(
+                            tuple.get(0,Long.class),
+                            tuple.get(1,String.class),
+                            tuple.get(2,Long.class)
+                    )
+            );
+        }
+        GraficoDTO graficoDTO = new GraficoDTO("Solicitação", lista);
+        return graficoDTO;
+    }
+
+    public GraficoDTO getGraficoEquipamentoSolicitacao(){
+        List<Tuple> dados = solicitationRepository.findGraficoEquipamentoSolicitacaoNative();
+        List<GraficoDadoDTO> lista = new ArrayList<>();
+        for (Tuple tuple : dados){
+            lista.add(
+                    new GraficoDadoDTO(
+                            tuple.get(0,Long.class),
+                            tuple.get(1,String.class).split(" ")[0],
+                            tuple.get(2,Long.class)
+                    )
+            );
+        }
+        GraficoDTO graficoDTO = new GraficoDTO("Equipamentos em solicitações", lista);
+        return graficoDTO;
+    }
+
+    public GraficoDTO getGraficoUsuarioSituacao(){
+        List<Tuple> dados = userRepository.findGraficoUsuarioSituacaoNative();
+        List<GraficoDadoDTO> lista = new ArrayList<>();
+        for (Tuple tuple : dados){
+            lista.add(
+                    new GraficoDadoDTO(
+                            tuple.get(0,Long.class),
+                            tuple.get(1,String.class),
+                            tuple.get(2,Long.class)
+                    )
+            );
+        }
+        GraficoDTO graficoDTO = new GraficoDTO("Usuários por situações", lista);
+        return graficoDTO;
+    }
+
+    public GraficoDTO getGraficoUsuarioRole(){
+        List<Tuple> dados = userRepository.findGraficoUsuarioTipoNative();
+        List<GraficoDadoDTO> lista = new ArrayList<>();
+        for (Tuple tuple : dados){
+            lista.add(
+                new GraficoDadoDTO(
+                        tuple.get(0,Long.class),
+                        tuple.get(1,String.class),
+                        tuple.get(2,Long.class))
+            );
+        }
+
+        GraficoDTO graficoDTO = new GraficoDTO("Usuários por tipo", lista);
+        return graficoDTO;
+    }
+
+    public GraficoDTO getGraficoEquipamentoSituacao(){
+        List<Tuple> dados = equipmentRepository.findGraficoEquipamentoSituacaoNative();
+
+        List<GraficoDadoDTO> lista = new ArrayList<>();
+        for (Tuple tuple : dados){
+            lista.add(
+                    new GraficoDadoDTO(
+                            tuple.get(0,Long.class),
+                            tuple.get(1,String.class),
+                            tuple.get(2,Long.class)
+                    )
+            );
+        }
+        GraficoDTO graficoDTO = new GraficoDTO("Equipamentos por situações", lista);
+        return graficoDTO;
+    }
+
+}
