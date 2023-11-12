a6414490d7fbc061a411b1a18f8dbe1269033b70
raulc <raul.cardoso@outlook.com.br>
2023-11-12 18:32:31 -0300

    Ajustada classe EmailModel para utilizar a auditoria;

--- src/main/java/com/portal/centro/API/model/EmailModel.java
+++ src/main/java/com/portal/centro/API/model/EmailModel.java
@@ -3,6 +3,10 @@ package com.portal.centro.API.model;
 import com.portal.centro.API.enums.StatusEmail;
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -10,6 +14,7 @@ import java.util.UUID;
 @Data
 @Entity
 @Table(name = "TB_EMAIL")
+@EntityListeners(AuditingEntityListener.class)
 public class EmailModel implements Serializable {
     private static final long serialVersionUID = 1L;
 
@@ -24,4 +29,12 @@ public class EmailModel implements Serializable {
     private String text;
     private LocalDateTime sendDateEmail;
     private StatusEmail statusEmail;
+
+    @Column(name = "created_at", updatable = false)
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column(name = "created_by", updatable = false)
+    @CreatedBy
+    private String createdby;
 }
