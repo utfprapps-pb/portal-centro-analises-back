4ebaf0ee216dd4cf2c40fc9c30c86260f3ec4e52
raulc <raul.cardoso@outlook.com.br>
2023-11-12 17:15:32 -0300

    Ajustada classe Equipament para utilizar a aduditoria;

--- src/main/java/com/portal/centro/API/model/Equipment.java
+++ src/main/java/com/portal/centro/API/model/Equipment.java
@@ -8,13 +8,21 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Entity(name = "equipment")
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Equipment {
 
     @Id
@@ -50,4 +58,20 @@ public class Equipment {
     private String shortName;
 
     private StatusInactiveActive status;
+
+    @Column(name = "created_at", updatable = false)
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column(name = "created_by", updatable = false)
+    @CreatedBy
+    private String createdby;
+
+    @Column(name = "modified_at")
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+
+    @Column(name = "modified_by")
+    @LastModifiedBy
+    private String modifiedby;
 }
