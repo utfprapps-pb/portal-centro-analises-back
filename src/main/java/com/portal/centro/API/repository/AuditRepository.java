c15647597f99e36f558ace91d1c0137602d81a79
GiovaneWerlang <giovanewerlang@alunos.utfpr.edu.br>
2023-11-13 21:58:25 -0300

    melhoria histórico, order change date desc

--- src/main/java/com/portal/centro/API/repository/AuditRepository.java
+++ src/main/java/com/portal/centro/API/repository/AuditRepository.java
@@ -16,13 +16,13 @@ public interface AuditRepository extends GenericRepository<Audit, Long> {
 
     List<Audit> findAllBySolicitation_CreatedBy(User user);
     Page<Audit> findAllBySolicitation_CreatedBy(User user, PageRequest pageRequest);
-    List<Audit> findAllBySolicitation_CreatedByAndSolicitationIdAndNewStatusIsNotOrderByNewStatus(User user, Long id, SolicitationStatus newStatus);
+    List<Audit> findAllBySolicitation_CreatedByAndSolicitationIdAndNewStatusIsNotOrderByChangeDateDesc(User user, Long id, SolicitationStatus newStatus);
 
     List<Audit> findAllBySolicitation_CreatedByOrSolicitation_Project_Teacher(User user, User teacher);
     Page<Audit> findAllBySolicitation_CreatedByOrSolicitation_Project_Teacher(User user, User teacher, PageRequest pageRequest);
-    List<Audit> findAllBySolicitation_CreatedByOrSolicitation_Project_TeacherAndSolicitationIdAndNewStatusIsNotOrderByNewStatus(User user, User teacher, Long id, SolicitationStatus newStatus);
+    List<Audit> findAllBySolicitation_CreatedByOrSolicitation_Project_TeacherAndSolicitationIdAndNewStatusIsNotOrderByChangeDateDesc(User user, User teacher, Long id, SolicitationStatus newStatus);
 
-    List<Audit> findAllBySolicitationIdAndNewStatusIsNotOrderByNewStatus(Long id, SolicitationStatus newStatus);
+    List<Audit> findAllBySolicitationIdAndNewStatusIsNotOrderByChangeDateDesc(Long id, SolicitationStatus newStatus);
 
     @Query(nativeQuery = true, value = "select a.* from audit a " +
            "where a.change_date in (select max(change_date) from audit group by solicitation_id) " +
--- src/main/java/com/portal/centro/API/service/AuditService.java
+++ src/main/java/com/portal/centro/API/service/AuditService.java
@@ -53,11 +53,11 @@ public class AuditService extends GenericService<Audit, Long> {
             case STUDENT:
             case EXTERNAL:
             case PARTNER:
-                return auditRepository.findAllBySolicitation_CreatedByAndSolicitationIdAndNewStatusIsNotOrderByNewStatus(user, id, status);
+                return auditRepository.findAllBySolicitation_CreatedByAndSolicitationIdAndNewStatusIsNotOrderByChangeDateDesc(user, id, status);
             case PROFESSOR:
-                return auditRepository.findAllBySolicitation_CreatedByOrSolicitation_Project_TeacherAndSolicitationIdAndNewStatusIsNotOrderByNewStatus(user, user, id, status);
+                return auditRepository.findAllBySolicitation_CreatedByOrSolicitation_Project_TeacherAndSolicitationIdAndNewStatusIsNotOrderByChangeDateDesc(user, user, id, status);
             case ADMIN:
-                return auditRepository.findAllBySolicitationIdAndNewStatusIsNotOrderByNewStatus(id, status);
+                return auditRepository.findAllBySolicitationIdAndNewStatusIsNotOrderByChangeDateDesc(id, status);
             default:
                 return new ArrayList<>();
         }
