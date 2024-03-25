e32f78cdab4d545dc47be416818696866155863e
Lucio <sganzerla.lucio1@gmail.com>
2024-03-25 20:24:31 -0300

    Fix: Melhorado tipo de retorno do ApiError

--- src/main/java/com/portal/centro/API/model/ApiError.java
+++ src/main/java/com/portal/centro/API/model/ApiError.java
@@ -3,6 +3,7 @@ package com.portal.centro.API.model;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
 import java.util.Map;
@@ -12,7 +13,8 @@ import java.util.Map;
 @JsonInclude(value = JsonInclude.Include.NON_NULL)
 public class ApiError {
     private LocalDateTime timestamp = LocalDateTime.now();
-    private int status;
+    private boolean mapped = false;
+    private int status = HttpStatus.INTERNAL_SERVER_ERROR.value();
     private String message;
     private String url;
     private Map<String, String> validationErrors;
@@ -30,4 +32,19 @@ public class ApiError {
         this.message = message;
         this.url = url;
     }
+
+    public ApiError(Exception exception, String url) {
+        this.mapped = true;
+        this.message = exception.getMessage();
+        this.url = url;
+    }
+
+    public ApiError(Exception exception, String url,
+                    Map<String, String> validationErrors) {
+        this.mapped = true;
+        this.message = exception.getMessage();
+        this.url = url;
+        this.validationErrors = validationErrors;
+    }
+
 }
