b3966d1e9735726ff69dcf34075cf6ed4f652ada
Lucio <sganzerla.lucio1@gmail.com>
2024-04-27 16:54:06 -0300

    Fix: Domain

--- src/main/java/com/portal/centro/API/controller/DomainRoleController.java
+++ src/main/java/com/portal/centro/API/controller/DomainRoleController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("domain-role")
+@RequestMapping("/domain-role")
 public class DomainRoleController extends GenericController<DomainRole, Long> {
 
     public DomainRoleController(DomainRoleService domainRoleService) {
--- src/main/java/com/portal/centro/API/security/filters/JWTAuthenticationFilter.java
+++ src/main/java/com/portal/centro/API/security/filters/JWTAuthenticationFilter.java
@@ -14,8 +14,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.io.IOException;
@@ -50,7 +52,7 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
                         )
                 );
             }
-        } catch (GenericException ge) {
+        } catch (UsernameNotFoundException | BadCredentialsException | GenericException ge) {
             throw new RuntimeException("mapped|GenericException|" + ge.getMessage());
         } catch (Exception e) {
             throw new RuntimeException(e);
