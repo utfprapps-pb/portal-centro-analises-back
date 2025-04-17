bb784e5350c2647ca540051f25694d8ea54bf4c9
Vinicius Pegorini <vinicius@utfpr.edu.br>
2025-04-17 20:37:14 -0300

    Update WebSecurity.java

--- src/main/java/com/portal/centro/API/security/WebSecurity.java
+++ src/main/java/com/portal/centro/API/security/WebSecurity.java
@@ -48,6 +48,9 @@ public class WebSecurity {
 
         http.csrf(AbstractHttpConfigurer::disable);
 
+        http.csrf(csrf -> csrf.ignoringRequestMatchers("/ws/**") // ou seu endpoint real
+                  );
+
         http.cors(cors -> corsConfigurationSource());
 
         http.exceptionHandling(exceptionHandling -> exceptionHandling.authenticationEntryPoint(authenticationEntryPoint));
