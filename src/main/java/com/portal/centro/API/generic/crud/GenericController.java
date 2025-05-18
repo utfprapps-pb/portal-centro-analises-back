2efe0d44714e880c4dbfdc416d9347bdcd08ed99
LucioSganzerla <sganzerla.lucio1@gmail.com>
2025-05-18 17:20:12 -0300

    Ajustes mensagem de erro

--- src/main/java/com/portal/centro/API/generic/crud/GenericController.java
+++ src/main/java/com/portal/centro/API/generic/crud/GenericController.java
@@ -1,25 +1,10 @@
 package com.portal.centro.API.generic.crud;
 
-import com.google.common.base.Joiner;
-import com.portal.centro.API.enums.SearchOperation;
-import com.portal.centro.API.generic.specification.GenericSpecification;
-import com.portal.centro.API.generic.specification.GenericSpecificationsBuilder;
-import com.portal.centro.API.model.ObjectReturn;
-import com.portal.centro.API.model.User;
 import jakarta.validation.Valid;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.domain.Specification;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.Serializable;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import static com.portal.centro.API.enums.Type.ROLE_ADMIN;
 
 public abstract class GenericController<T extends GenericModel, ID extends Serializable> {
 
